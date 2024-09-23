1047b933447ab17c2da00dd82ab203c47d91245a
Rodrigo Carbonel <rcarbonel4471@gmail.com>
2024-09-22 22:17:03 -0500

    Carga inicial del proyecto

---
+++ src/main/java/com/edu/cibertec/examen_frontend_b/ExamenFrontendBApplication.java
@@ -0,0 +1,13 @@
+package com.edu.cibertec.examen_frontend_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFrontendBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFrontendBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/cibertec/examen_frontend_b/ExamenFrontendBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.cibertec.examen_frontend_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFrontendBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
